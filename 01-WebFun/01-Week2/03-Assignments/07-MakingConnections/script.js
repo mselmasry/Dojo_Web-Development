6488704cda80ff0d88ac0b442e3cf2c11f7f6bae
mThirteen <m@masry.me>
2022-08-09 18:49:37 +0200

    Finalize Making Connections

--- 01-WebFun/01-Week2/03-Assignments/07-MakingConnections/script.js
+++ 01-WebFun/01-Week2/03-Assignments/07-MakingConnections/script.js
@@ -1,4 +1,39 @@
+var reqcount = 2;
+var conncount = 418;
+
 function changeName()
 {
     document.querySelector("#title").innerText = "John Doe";
+}
+
+function accrequest1()
+{
+    element = document.querySelector("#req1").remove();
+    reqcount--;
+    conncount++;
+    document.querySelector("#reqcount").innerText = reqcount;
+    document.querySelector("#conncount").innerText = conncount;
+}
+
+function rejrequest1()
+{
+    element = document.querySelector("#req1").remove();
+    reqcount--;
+    document.querySelector("#reqcount").innerText = reqcount;
+}
+
+function accrequest2()
+{
+    element = document.querySelector("#req2").remove();
+    reqcount--;
+    conncount++;
+    document.querySelector("#reqcount").innerText = reqcount;
+    document.querySelector("#conncount").innerText = conncount;
+}
+
+function rejrequest2()
+{
+    reqcount--;
+    element = document.querySelector("#req2").remove();
+    document.querySelector("#reqcount").innerText = reqcount;
 }
